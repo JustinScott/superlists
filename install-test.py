5174aa26d7332dc2e9f22cb5ce0ced8ca9d0068a
Justin <jsco976@gmail.com>
2017-02-20 20:53:17 -0600

    added setup/teardown methods using fixtures

--- install-test.py
+++ install-test.py
@@ -1,7 +1,17 @@
+import pytest
 from selenium import webdriver
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+@pytest.fixture(scope='module')
+def browser():
+    #setup
+    _browser = webdriver.Firefox()
+    yield _browser
+    #teardown
+    _browser.quit()
 
-assert "Django" in browser.title
+
+def test_can_start_a_list_and_retrieve_it_later(browser):
+    browser.get('http://localhost:8000')
+    assert "To-Do" in browser.title
+    pytest.fail("Finish writing the test.")
